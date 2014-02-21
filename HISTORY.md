ab0506afa368936d8820f919d1b29b5a9daada8b
chanh <wpqh918@gmail.com>
2014-02-21 16:56:25 +0800

    上传文件

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+# History
+
+---
+
+## 1.0.0
+
+`new` seaui/base First version.
---
+++ tests/base-spec.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+  var base = require('../src/base');
+
+  describe('base', function() {
+
+    it('normal usage', function() {
+
+    });
+  });
+
+});
